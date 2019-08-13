5f32a1473b3b6b7866a6b6c0dced9e9e1c22cb93
rio <mario64aq@gmail.com>
2019-08-13 23:34:10 +0900

    Modify improve php perfomance
    change " to '

--- checkCnt.php
+++ checkCnt.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT');
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
-$dbConnect = mysqli_connect("localhost","root","autoset");
+$dbConnect = mysqli_connect('localhost','root','autoset');
 mysqli_select_db($dbConnect,'parkcnt');
 $today  = date('Y-m-d');
 echo $today;
-$query = " select * from parktable where  date = '$today' ";
+$query = "select * from parktable where  date = '$today' ";
 $result = mysqli_query($dbConnect,$query);
 //if($result-> num_rows > 0){
     // while ($row = mysqli_fetch_assoc($result)){
@@ -17,7 +17,7 @@ $result = mysqli_query($dbConnect,$query);
 //if(!$result || mysqli_num_rows($result) == 0 ){// num_rows 는 fasle 시 error 발생
     if(!$result || mysqli_num_rows($result) > 0 ){// num_rows 는 fasle 시 error 발생
     while($row = mysqli_fetch_assoc($result)) {
-    echo "<br>대: " . $row["big"]. " md: " . $row["mid"]. "sm: " . $row["small"]. " total: " . $row["total"] . "<br>";
+    echo "<br>대: " . $row['big']. " md: " . $row['mid']. "sm: " . $row['small']. " total: " . $row['total'] . "<br>";
     }
 }
 mysqli_close($dbConnect);
--- cnt.php
+++ cnt.php
@@ -1,16 +1,16 @@
-<?
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT');
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
 
 $big =$_POST['inBig'];
 $md = $_POST['inMd'];
 $sm = $_POST['inSm'];
 $total =  $_POST['inTotal'];
 
-$dbConnect = mysqli_connect("localhost","root","autoset");
-	mysqli_select_db($dbConnect,"parkcnt");
-	$today = date("Ymd");
+$dbConnect = mysqli_connect('localhost','root','autoset');
+	mysqli_select_db($dbConnect,'parkcnt');
+	$today = date('Ymd');
 	echo $today;
 	$query = "insert into parktable (total,big,mid,small,date) values ($total,$big,$md,$sm, $today)";
 	
