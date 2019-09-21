8871b7cc2902098b42734ae90dd43dc97da46406
kit rio <mario64aq@gmail.com>
2019-09-21 16:12:18 +0900

    Modify code indent

--- js/chartCnt.js
+++ js/chartCnt.js
@@ -1,71 +1,71 @@
-		let timeArr = ["x"];
-		let smArr = ["sm"];
-		let mdArr = ["md"];
-		let bigArr = ["big"];
-		let totalArr = ["total"];
-		function monthChk(){
-			axios({
-				method: 'POST',
-				url: 'http://localhost/monthChk.php',
-				headers: {
-					"Content-Type": "application/json",
-				  }
-			})
-			.then((res)=>{
-				const arrLength = res.data.length;
-				for(let i=0;i<arrLength;++i){
-					timeArr.push( res.data[i].date);
-					smArr.push(res.data[i].sm);
-					mdArr.push(res.data[i].md);
-					bigArr.push(res.data[i].big);
-					totalArr.push(res.data[i].total);
-				}
-			}).then(()=>{
-				displayChart();
-			})
+let timeArr = ["x"];
+let smArr = ["sm"];
+let mdArr = ["md"];
+let bigArr = ["big"];
+let totalArr = ["total"];
+function monthChk() {
+	axios({
+		method: 'POST',
+		url: 'http://localhost/monthChk.php',
+		headers: {
+			"Content-Type": "application/json",
+		}
+	})
+		.then((res) => {
+			const arrLength = res.data.length;
+			for (let i = 0; i < arrLength; ++i) {
+				timeArr.push(res.data[i].date);
+				smArr.push(res.data[i].sm);
+				mdArr.push(res.data[i].md);
+				bigArr.push(res.data[i].big);
+				totalArr.push(res.data[i].total);
+			}
+		}).then(() => {
+			displayChart();
+		})
 
-		};
-		
-		monthChk();
+};
 
-		function displayChart(){
-		const chart = bb.generate({
-			data: {
-				x: "x",
-				xFormat: "%Y-%m-%d",
-				columns: [
-					timeArr,
-					smArr,//["sm", 0, 2, 0, 3],
-					mdArr,//["md", 4, 3, 5, 2],
-					bigArr//["big",0, 1, 2, 2]
-				],
-				type: document.querySelector('input[name="graphType"]:checked').value//graphTypeSelector()
+monthChk();
+
+function displayChart() {
+	const chart = bb.generate({
+		data: {
+			x: "x",
+			xFormat: "%Y-%m-%d",
+			columns: [
+				timeArr,
+				smArr,//["sm", 0, 2, 0, 3],
+				mdArr,//["md", 4, 3, 5, 2],
+				bigArr//["big",0, 1, 2, 2]
+			],
+			type: document.querySelector('input[name="graphType"]:checked').value//graphTypeSelector()
+		},
+		bar: {
+			width: {
+				ratio: 1.0
 			},
-			bar: {
-				width: {
-					ratio: 1.0
-				},
-				height: {
-					ratio: 1
+			height: {
+				ratio: 1
+			}
+		},
+		axis: {
+			x: {
+				type: "timeseries",
+				localtime: true,
+				tick: {
+					format: "%Y %m-%d"
 				}
-			},
-			axis: {
-				x: {
-					type: "timeseries",
-					localtime: true,
-					tick: {
-						format: "%Y %m-%d"
-					}
-				}
-			},
-			bindto: "#myChart"
+			}
+		},
+		bindto: "#myChart"
+	});
+	setTimeout(function () {
+		chart.load({
+			columns: [
+				totalArr
+			],
+			type: "line"
 		});
-		setTimeout(function(){
-			chart.load({
-				columns: [
-					totalArr
-				],
-				type: "line"
-                });
-		    },800);
-	}
+	}, 800);
+}
--- js/counter.js
+++ js/counter.js
@@ -1,32 +1,32 @@
-const btnFetch = document.getElementById ("btnFectch");
+const btnFetch = document.getElementById("btnFectch");
 const btnToday = document.getElementById("btnToday");
-function counter(name){
+function counter(name) {
     let a = document.getElementById(name).value;
     document.getElementById(name).value = ++a;
 }
-    
-    btnFetch.addEventListener ("click",
-        () => {
-            let form  = new FormData(document.getElementById("formCnt"));
-            axios({
-                method: "POST",
-                headers: {
-					'Access-Control-Allow-Origin' : '*',
-                    'Accept' : 'application/json, text/plain'
-				  },
-                url: 'http://localhost/cnt.php',
-                data: form
-                })
+
+btnFetch.addEventListener("click",
+    () => {
+        let form = new FormData(document.getElementById("formCnt"));
+        axios({
+            method: "POST",
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Accept': 'application/json, text/plain'
+            },
+            url: 'http://localhost/cnt.php',
+            data: form
+        })
             .then(console.log(form))
-            .catch(e=>console.log(e));
+            .catch(e => console.log(e));
     });
 
-    btnToday.addEventListener("click",
-        function(){
-            axios.get('http://localhost/checkCnt.php')
+btnToday.addEventListener("click",
+    function () {
+        axios.get('http://localhost/checkCnt.php')
             .then((res) => {
                 document.querySelector('article').innerHTML = res.data
                 console.dir(res)
             })
             .catch(e => console.log(e));
-        });
+    });
